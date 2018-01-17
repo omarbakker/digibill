67de7d56fc09f86d406be1ecfb8765bc93c7ffac
omarbakker <v0l0b@ece.ubc.ca>
2018-01-16 22:21:05 -0800

    Model almost done;

---
+++ src/network-trainer/model.py
@@ -0,0 +1,231 @@
+import utils
+import tensorflow as tf
+
+learningRate = 1e-4
+epochs = 3 # since we can generate as much data as we need
+batchSize = utils.batchSize
+lstmSize = 2**9
+pkeepi = 0.5
+pkeepConvi = 0.7
+pkeepLSTMi = 0.8
+seqlen = 100
+logdir = './log'
+checkpointdir = './checkpoint'
+restore = True # restore from checkpoint
+nClasses = utils.nClasses
+
+params = utils.params
+imgWidth = int(params['width'])
+imgHeight = int(params['height'])
+channels = 1
+
+# this will hold the input to the CNN
+x = tf.placeholder(tf.float32, [None, imgWidth * imgHeight])
+x = tf.reshape(x, [-1, imgHeight, imgWidth, channels])
+
+# this will hold the output of the entire network
+y = tf.sparse_placeholder(tf.int32)
+
+# learning rate placeholder
+lr = tf.placeholder(tf.float32)
+
+# test flag for batch normalization
+tst = tf.placeholder(tf.bool)
+itera = tf.placeholder(tf.int32)
+
+# dropout probs
+pkeep = tf.placeholder(tf.float32)
+pkeepConv = tf.placeholder(tf.float32)
+pkeepLSTM = tf.placeholder(tf.float32)
+
+# list to hold moving exponential moving average ops
+ema = []
+
+def relu(x_in):
+    return tf.nn.relu(x_in)
+
+def dropout(x_in, isConv=False):
+    if isConv:
+        return tf.nn.dropout(x_in, pkeepConv, compatibleConvNoiseShape(x_in))
+    else:
+        return tf.nn.dropout(x_in, pkeep)
+
+def conv(x_in, channels, filters, filterShape, name):
+    convFilterShape = [filterShape[0], filterShape[1], channels, filters]
+    w = tf.Variable(tf.truncated_normal(convFilterShape, stddev=0.03),
+                                      name=name+'_W')
+    b = tf.Variable(tf.truncated_normal([filters]), name=name+'_b')
+    out = tf.nn.conv2d(x_in, w, strides=[1,1,1,1], padding='SAME')
+    return out + b, b
+
+def pool(x_in, shape, strides):
+    ksize = [1, shape[0], shape[1], 1]
+    return tf.nn.max_pool(x_in, ksize=ksize, strides=strides, padding='SAME')
+
+def dense(x_in, shape, name):
+    w_i = tf.truncated_normal(shape, stddev=0.03)
+    b_i = tf.truncated_normal([shape[-1]], stddev=0.01)
+    w = tf.Variable(w_i, name=name+'_W')
+    b = tf.Variable(b_i, name=name+'_b')
+    return tf.matmul(x_in, w) + b, b
+
+def batchNorm(x_in, isTest, iteration, offset, isConv=False):
+    # adapted from sarvesh278 kernel on kaggle
+    emv = tf.train.ExponentialMovingAverage(0.999, iteration)
+    bnepsilon = 1e-5
+    if isConv:
+        mean, variance = tf.nn.moments(x_in, [0, 1, 2])
+    else:
+        mean, variance = tf.nn.moments(x_in, [0])
+    update_moving_everages = emv.apply([mean, variance])
+    m = tf.cond(isTest, lambda: emv.average(mean), lambda: mean)
+    v = tf.cond(isTest, lambda: emv.average(variance), lambda: variance)
+    Ybn = tf.nn.batch_normalization(x_in, m, v, offset, None, bnepsilon)
+    ema.append(update_moving_everages)
+    return Ybn
+
+def compatibleConvNoiseShape(Y):
+    noiseshape = tf.shape(Y)
+    noiseshape = noiseshape * tf.constant([1,0,0,1])\
+                            + tf.constant([0,1,1,0])
+    return noiseshape
+
+def bilstm(bottom_sequence,sequence_length,rnn_size,scope):
+    """Build bidirectional (concatenated output) RNN layer"""
+
+    weight_initializer = tf.truncated_normal_initializer(stddev=0.01)
+    lstmCell = tf.contrib.rnn.LSTMCell
+    DropoutWrapper = tf.contrib.rnn.DropoutWrapper
+    cell_fw = lstmCell(rnn_size, initializer=weight_initializer)
+    cell_bw = lstmCell(rnn_size, initializer=weight_initializer)
+    cell_fw = DropoutWrapper(cell_fw, input_keep_prob=pkeepLSTMi)
+    cell_bw = DropoutWrapper(cell_bw, input_keep_prob=pkeepLSTMi)
+
+    rnn_output,_ = tf.nn.bidirectional_dynamic_rnn(
+        cell_fw, cell_bw, bottom_sequence,
+        # sequence_length=seqlen,
+        time_major=True,
+        dtype=tf.float32,
+        scope=scope)
+    # return tf.concat(rnn_output,2,name='output_stack')
+    return rnn_output
+
+def convolutionalLayers(x_in):
+    #1 conv -> batchnorm -> relu -> dropout -> pool
+    conv1, b1 = conv(x_in, 1, 64, [5,5], name='conv1')
+    norm1 = batchNorm(conv1, tst, itera, b1, isConv=True)
+    relu1 = relu(norm1)
+    drop1 = dropout(relu1, isConv=True)
+    pool1 = pool(drop1, shape=[3,3], strides=[1, 3, 3, 1])
+
+    #2 conv -> batchnorm -> relu -> dropout -> pool
+    conv2, b2 = conv(pool1, 64, 128, [5,5], name='conv2')
+    norm2 = batchNorm(conv2, tst, itera, b2, isConv=True)
+    relu2 = relu(norm2)
+    drop2 = dropout(relu2, isConv=True)
+    pool2 = pool(drop2, shape=[3,3], strides=[1, 3, 3, 1])
+
+    #3 conv -> batchnorm -> relu -> dropout -> pool
+    conv3, b3 = conv(pool2, 128, 96, [5,5], name = 'conv3')
+    norm3 = batchNorm(conv3, tst, itera, b3, isConv=True)
+    relu3 = relu(norm3)
+    drop3 = dropout(relu3, isConv=True)
+    pool3 = pool(drop3, shape=[2,2], strides=[1, 2, 2, 1])
+    features = tf.reshape(pool3, [-1, 16 * 2 * 96])
+
+    return features
+
+def denseLayers(x_in):
+    #4 dense -> batchnorm -> relu -> dropout
+    dense1, bd1 = dense(x_in, shape=[16 * 2 * 96, 1000], name='dense1')
+    normd1 = batchNorm(dense1, tst, itera, bd1)
+    relud1 = relu(normd1)
+    dropd1 = dropout(relud1)
+
+    #5 dense -> batchnorm -> relu -> dropout
+    dense2, bd2 = dense(dropd1, shape=[1000, seqlen], name='dense2')
+    normd2 = batchNorm(dense2, tst, itera, bd2)
+    relud2 = relu(normd2)
+    dropd2 = dropout(relud2) # seq len = 100 ?
+
+    return dropd2
+
+def recurrentLayers(x_in):
+    x_in = tf.reshape(x_in, [batchSize, -1, seqlen])
+    x_in = tf.transpose(x_in, [0, 2, 1])
+    x_in.set_shape([batchSize, seqlen, 50])
+
+    #6 bilstm -> bilstm -> dense -> relu
+    lstm1 = bilstm(x_in, seqlen, lstmSize, "bilstm1")
+    lstm2 = bilstm(lstm1, seqlen, lstmSize, "bilstm2")
+
+    # final dense
+    # print(tf.shape(lstm2))
+    lstm2 = tf.reshape(lstm2, [-1, lstmSize])
+    out, _ = dense(lstm2, [lstmSize, nClasses + 1], 'out')
+
+    shape = tf.shape(x_in)
+    y_ = tf.reshape(relu(out), [shape[0], -1, nClasses + 1])
+    y_ = tf.transpose(y_, (1, 0, 2))
+
+    return y_
+
+def CTCLoss(y_in, y_):
+    #7 Connectionist Temporal Classification
+    loss = tf.nn.ctc_loss(labels=y_in, inputs=y_,
+                            sequence_length=seqlen, time_major=True)
+    meanLoss = tf.reduce_mean(loss)
+    return meanLoss
+
+def buildModel(x_in, y_in):
+    features = convolutionalLayers(x_in)
+    sequenceIn = denseLayers(features)
+    logits = recurrentLayers(sequenceIn)
+    loss = CTCLoss(y_in, logits)
+    trainStep = tf.train.AdamOptimizer(lr).minimize(loss)
+    decoder = tf.nn.ctc_beam_search_decoder
+    decoded, probs = decoder(logits, seqlen, merge_repeated=False)
+    dense_decoded = tf.sparse_tensor_to_dense(decoded[0], default_value=-1)
+    return trainStep, dense_decoded
+
+with tf.Session() as sess:
+
+    trainOp, y_out = buildModel(x, y)
+    ema = tf.group(ema)
+
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver(tf.global_variables(), max_to_keep=100)
+    writer = tf.summary.FileWriter(logdir + '/train', sess.graph)
+
+    if restore:
+        chkpt = tf.train.latest_checkpoint(checkpointdir)
+        if chkpt:
+            print('restoring checkpoint: {}'.format(chkpt))
+            saver.restore(sess, chkpt)
+
+    print('=========================Training Begins!=========================')
+
+    for epochNum in range(epochs):
+        print('Starting epoch {}'.format(epochNum+1))
+        batchNum = 0
+
+        for batch, labels in utils.batches():
+            batchNum += 1
+            iteration = (batchSize*(epochNum)) + batchNum
+
+            feed = {x: batch, y:labels, lr:learningRate,
+                    pkeep:pkeepi, pkeepConv:pkeepConvi, pkeepLSTM:pkeepLSTMi}
+            feedema = {x: batch, y: labels, tst: False, itera: iteration,
+                        pkeep: 1.0, pkeepConv: 1.0, pkeepLSTM:1.0}
+
+            sess.run(trainOp, feed)
+            sess.run(ema, feedema)
+
+            print('Finished batch {}:'.format(batchNum))
+
+
+
+
+
+
+# end
--- src/network-trainer/utils.py
+++ src/network-trainer/utils.py
@@ -1,14 +1,57 @@
+import numpy as np
+import cv2
+
+conf = open('../data-generator/params.conf').readlines()
+params = [line.split(' = ') for line in conf if line[0] is not '#']
+params = [param for param in params if len(param) is 2]
+params = dict(zip([p[0].strip() for p in params],\
+                  [p[1].strip() for p in params]))
+
+imgDir = '../data-generator/images/'
+imgWidth = int(params['width'])
+imgHeight = int(params['height'])
+channels = 1
+
+testPercentage = 0.05
+batchSize = 50
+labels = open(imgDir + 'labels.txt').readlines()
+labels = [l.rstrip('\n').rstrip(' ') for l in labels]
+labels = [l.split(':') for l in labels]
+images = [l[0].rstrip() for l in labels]
+testImgCount = int(len(images) * testPercentage)
+testImages = images[-testImgCount:]
+images = images[:-testImgCount]
+print('Image count: {}'.format(len(images) + len(testImages)))
+labelMap = dict(zip(images, [l[1].rstrip() for l in labels]))
+
+def shuffle(lst):
+    return list(np.array(lst)[np.random.permutation(len(lst))])
+
+def encodeLabel(label):
+    return [encodings.get(c) for c in list(label) if c in encodings]
+
+def getLabel(img):
+    if img not in labelMap: print("Error: {} label not found".format(img))
+    return labelMap.get(img)
+
+def readImage(img):
+    img = cv2.imread(img, cv2.IMREAD_GRAYSCALE).astype(np.float32)/255.
+    img = np.reshape(img, [imgHeight, imgWidth, channels])
+    return img
+
+def batches():
+    shuffle(images);
+    batchSize = batchSize
+    numbatches = images // batchSize
+    for i in range(numbatches):
+        batchImgs = images[i * batchSize: (i+1) * batchSize]
+        encodedLabels = [encodeLabel(getLabel(img)) for img in batchImgs]
+        batchImgs = [readImage(img) for img in batchImgs]
+        yield batchImgs, encodedLabels
 
 
-def readConf():
-    conf = open('../data-generator/params.conf').readlines()
-    params = [line.split(' = ') for line in conf if line[0] is not '#']
-    params = [param for param in params if len(param) is 2]
-    params = dict(zip([p[0].strip() for p in params],\
-                      [p[1].strip() for p in params]))
-    return params
 
-def getOutputEncodings(verbose=False):
+def getOutputMaps(verbose=False):
     corpus = open('../data-generator/processed-corpus.txt').read().split('\n')
     corpus = list(' '.join(corpus))
     corpus = [char for char in corpus if char is not '\t']
@@ -24,6 +67,24 @@ def getOutputEncodings(verbose=False):
         for i in range(50):
             print(countsList[i])
         print(len(countsList))
-    mapping = dict(zip([c[0] for c in countsList],\
-                        range(len(countsList))))
-    return mapping
+    encodings = dict(zip([c[0] for c in countsList if c[0] is not ' '],
+                        range(1, len(countsList))))
+    decodings = dict(zip(range(1, len(countsList)),
+                        [c[0] for c in countsList if c[0] is not ' ']))
+    encodings[' '] = 0
+    decodings[0] = ' '
+    return encodings, decodings
+
+def dictToString(d):
+    items = [':'.join([str(i) for i in item]) for item in list(d.items())]
+    return '\n'.join(items)
+
+def saveOutputMaps(encodings, decodings):
+    with open('../data-generator/encodings.txt', 'w') as enc:
+        enc.write(dictToString(encodings))
+    with open('../data-generator/decodings.txt', 'w') as dec:
+        dec.write(dictToString(decodings))
+
+encodings, decodings = getOutputMaps()
+nClasses = len(encodings) - 1 # -1 because space is not a class
+saveOutputMaps(encodings, decodings)
